6d31722005c4a06b35dff3c4d18539c1de8b7a53
Hannah Pulmano <hannah.pulmano865@gmail.com>
2025-05-07 20:13:16 +0800

    flame sensor added

--- CaseStudy/CaseStudy.ino
+++ CaseStudy/CaseStudy.ino
@@ -2,21 +2,24 @@
 
 LCD_I2C lcd(0x27);
 
-const int flameSensorPin = 2;
-const int buzzerPin = 3;
-const int ledPin1 = 4;
-const int ledPin2 = 5;
-const int ledPin3 = 6;
+const int flamePinLft = 2;
+const int flamePinRgt = 3;
+const int buzzerPin = 4;
+const int ledPin1 = 5;
+const int ledPin2 = 6;
 
-int currentState = 1;
-int lastState = 1;
+int currentStateLft = 1;
+int lastStateLft = 1;
+
+int currentStateRgt = 1;
+int lastStateRgt = 1;
 
 void setup() {
-  pinMode(flameSensorPin, INPUT);
+  pinMode(flamePinLft, INPUT);
+  pinMode(flamePinRgt, INPUT);
   pinMode(buzzerPin, OUTPUT);
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
-  pinMode(ledPin3, OUTPUT);
 
   lcd.begin();
   lcd.backlight();
@@ -27,18 +30,39 @@ void setup() {
 
 void loop() {
   // No flame input is 1.
-  currentState = digitalRead(flameSensorPin);
-
-  if (currentState != lastState) {
-    if(currentState == 0) {
+  currentStateLft = digitalRead(flamePinLft);
+  currentStateRgt = digitalRead(flamePinRgt);
+   
+  if ((currentStateLft != lastStateLft) || (currentStateRgt != lastStateRgt)) {
+    if(currentStateLft == 0 && currentStateRgt == 0) {
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Fire!");
+      lcd.print("Fire on both!");
+      delay(1000);
 
       tone(buzzerPin, 440);
       digitalWrite(ledPin1, HIGH);
       digitalWrite(ledPin2, HIGH);
-      digitalWrite(ledPin3, HIGH);
+
+    } else if(currentStateLft == 0) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Fire on the lft!");
+      delay(1000);
+
+      tone(buzzerPin, 440);
+      digitalWrite(ledPin1, HIGH);
+      digitalWrite(ledPin2, HIGH);
+
+    } else if(currentStateRgt == 0) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Fire on the rgt!");
+      delay(1000);
+
+      tone(buzzerPin, 440);
+      digitalWrite(ledPin1, HIGH);
+      digitalWrite(ledPin2, HIGH);
     } else{
       lcd.clear();
       lcd.setCursor(0, 0);
@@ -47,11 +71,11 @@ void loop() {
       noTone(buzzerPin);
       digitalWrite(ledPin1, LOW);
       digitalWrite(ledPin2, LOW);
-      digitalWrite(ledPin3, LOW);
     }
 
     delay(50);
   }
 
-  lastState = currentState;
+  lastStateLft = currentStateLft;
+  lastStateRgt = currentStateRgt;
 }
